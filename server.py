8773636f5d32cebb372fe2f952881234b99305de
kranonetka <fakemail4steam@ya.ru>
2019-04-17 12:29:14 +0700

    [#] Shared dict fix

--- server.py
+++ server.py
@@ -2,15 +2,12 @@ import socket
 from multiprocessing import Process, Manager
 
 
-data = Manager.dict()
-
-
 #set <key> : <val>
 #get <key>
 #del <key>
 
 
-def main(connection):
+def main(storage, connection):
 	while True:
 		cmd = b""
 		while True:
@@ -21,19 +18,23 @@ def main(connection):
 		cmd = cmd.decode()
 		if cmd.startswith("set "):
 			pass
-		if cmd.startswith("get "):
+		elif cmd.startswith("get "):
 			pass
-		if cmd.startswith("del "):
+		elif cmd.startswith("del "):
 			pass
+		else:
+			pass #unrecognized command
 
 
 if __name__ == "__main__":
+	storage = Manager().dict()
+
 	sock = socket.socket()
 	sock.bind(("", 9090))
 	sock.listen(5)
 	while True:
 		conn, addr = sock.accept()
 		print("New connection from {}".format(addr))
-		proc = Process(target=main, args=(conn,))
+		proc = Process(target=main, args=(storage, conn))
 		proc.daemon = True
 		proc.start()
